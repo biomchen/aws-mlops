20dd6c35fa1279613ece822b28bbfbabb0fa5d8c
Meng Chen <meng.chen03@gmail.com>
2024-04-07 15:33:46 -0400

    added glue_etl.py file

---
+++ basic_flow/glue_etl.py
@@ -0,0 +1,86 @@
+
+import sys
+
+from pyspark.context import SparkContext
+
+from awsglue.context import GlueContext
+from awsglue.dynamicframe import DynamicFrame
+from awsglue.job import Job
+from awsglue.transforms import *
+from awsglue.utils import getResolvedOptions
+
+from constants import ARGS
+
+class JobCreator:
+
+    def __init__(self):
+        self.args = getResolvedOptions(sys.argv, ARGS)
+        self.glueContext, self.spark = self.initialize_glue()
+
+    def initialize_glue(self):
+        sc = SparkContext()
+        glueContext = GlueContext(sc)
+        spark = glueContext.spark_session
+        return glueContext, spark
+    
+    def initialize_job(self):
+        job = Job(self.glueContext)
+        job.init(self.args['JOB_NAME'], self.args)
+        return job
+    
+    def load_source_data(self):
+        return self.spark.read.load(
+            self.args['S3_SOURCE'],
+            format='csv',
+            infraSchema=True,
+            header=False
+        )
+    
+    def process_job(self):
+        job = self.initialize_job()
+        self.create_train_test_data()
+        job.commit()
+
+    def create_train_test_data(self):
+       data = self.load_source_data()
+       train_data, test_data = data.randomSplit([0.7, 0.3])
+       train_path = self.args['S3_DEST'] + self.args['TRAIN_KEY']
+       test_path = self.args['S3_DEST'] + self.args['VAL_KEY']
+       train_data.write.save(train_path, format='csv', model='overwrite')
+       test_data.write.save(test_path, format='csv', model='overwrite')
+
+JobCreator().process_job()
+
+# # Retrieve parameters for the Glue job.
+# args = getResolvedOptions(
+#     sys.argv, 
+#     ["JOB_NAME", "S3_SOURCE", "S3_DEST", "TRAIN_KEY", "VAL_KEY"]
+# )
+
+# sc = SparkContext()
+# glueContext = GlueContext(sc)
+# spark = glueContext.spark_session
+# job = Job(glueContext)
+# job.init(args["JOB_NAME"], args)
+
+# # Create a PySpark dataframe from the source table.
+# source_data_frame = spark.read.load(
+#     args["S3_SOURCE"], 
+#     format="csv", 
+#     inferSchema=True, 
+#     header=False
+# )
+
+# # Split the dataframe in to training and validation dataframes.
+# train_data, val_data = source_data_frame.randomSplit([0.7, 0.3])
+
+# # Write both dataframes to the destination datastore.
+# train_path = args["S3_DEST"] + args["TRAIN_KEY"]
+# val_path = args["S3_DEST"] + args["VAL_KEY"]
+
+# train_data.write.save(train_path, format="csv", mode="overwrite")
+# val_data.write.save(val_path, format="csv", mode="overwrite")
+
+# # Complete the job.
+# job.commit()
+
